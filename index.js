16527b58ab3e0432243ddcdd963ca204d0d53467
Eric <eric.rose@gmail.com>
2015-08-28 11:01:35 -0400

    db stuff

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var cool = require('cool-ascii-faces');
 var express = require('express');
+var mysql = require('mysql');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -11,7 +12,22 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  var result = ''
+  var times = process.env.TIMES || 5
+  for (i=0; i < times; i++)
+  	result += cool();
+  response.send(result);
+});
+
+app.get('/db', function(request, response){
+
+	var connection = mysql.createConnection(process.env.DATABASE_URL);
+
+	connection.query('SELECT * FROM mark_test', function(err, result){
+	  console.log(result);
+	  
+	  response.render('pages/db', {results: result});
+	});
 });
 
 app.get('/cool', function(request, response){
